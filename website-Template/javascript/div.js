dc932121cc9c757ba95c1ea4e9af049a65cd5d26
Md Arefin <arefin.shahrear@gmail.com>
2015-08-18 08:50:33 -0400

    Merge remote-tracking branch 'citytechcia/master'
    Conflicts:
    	assets/img/event/cia_members_fall_2014.png
    	assets/img/event/cia_team_hackathon_fall_2014.png
    	contact.html
    	dist/css/custom.css
    	gallery.html
    	index.html
    	portfolio.html
    	project.html
    	website-Template/protfofio.html

--- website-Template/javascript/div.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready( function(){
-  $( "#slow" ).show ( "slow" );
-  $( "#fast" ).show ( "fast" );
-  $( "#ms" ).show( 1500 );
-  $( "#toggle" ).on( "click" , function(){
-    $( "#toggled" ).toggle();
-  });
-});
